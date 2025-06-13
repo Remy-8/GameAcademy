6f04162c5542cb5d6cb152fc93b925ada635dd95
Remy-8 <emillopez2325@gmail.com>
2025-06-12 22:54:43 -0400

    Implement basic CRUD API for Challenge entity
    Add Challenge model, AppDbContext, and ChallengesController with CRUD API

---
+++ Homework2/src/GamifiedProgrammingAcademy/GamifiedProgrammingAcademy.API/Controllers/ChallengesController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GamifiedProgrammingAcademy.API.Data;     
+using GamifiedProgrammingAcademy.API.Entities; 
+using System.Collections.Generic;              
+using System.Threading.Tasks;                  
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class ChallengesController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ChallengesController(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Challenge>>> GetChallenges()
+    {
+        return await _context.Challenges.ToListAsync();
+    }
+
+    
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Challenge>> GetChallenge(int id)
+    {
+        var challenge = await _context.Challenges.FindAsync(id);
+        if (challenge == null) return NotFound();
+        return challenge;
+    }
+
+    
+    [HttpPost]
+    public async Task<ActionResult<Challenge>> CreateChallenge(Challenge challenge)
+    {
+        _context.Challenges.Add(challenge);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetChallenge), new { id = challenge.Id }, challenge);
+    }
+
+    
+    [HttpPut("{id}")]
+    public async Task<IActionResult> UpdateChallenge(int id, Challenge challenge)
+    {
+        if (id != challenge.Id) return BadRequest();
+        _context.Entry(challenge).State = EntityState.Modified;
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+
+    
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteChallenge(int id)
+    {
+        var challenge = await _context.Challenges.FindAsync(id);
+        if (challenge == null) return NotFound();
+        _context.Challenges.Remove(challenge);
+        await _context.SaveChangesAsync();
+        return NoContent();
+    }
+}
---
+++ Homework2/src/GamifiedProgrammingAcademy/GamifiedProgrammingAcademy.API/Entities/Challenge.cs
@@ -0,0 +1,11 @@
+﻿namespace GamifiedProgrammingAcademy.API.Entities
+{
+    public class Challenge
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Points { get; set; }
+    }
+
+}
